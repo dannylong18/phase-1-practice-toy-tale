b5b2531e78a2c6e5d5ce5f86401eb66e706dbad6
Daniel Long <danielwlong618@gmail.com>
2023-10-04 20:17:40 -0500

    lab done

--- src/index.js
+++ src/index.js
@@ -12,4 +12,134 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  fetch('http://localhost:3000/toys')
+  .then((resp) => resp.json())
+  .then((data) => {
+    data.forEach((toy) => {
+      let divCard = document.createElement('div')
+      divCard.className = 'card'
+      document.getElementById('toy-collection').appendChild(divCard)
+
+      let toyNameH2 = document.createElement('h2')
+      toyNameH2.innerText = toy.name 
+      divCard.appendChild(toyNameH2)
+
+      let toyImage = document.createElement('img')
+      toyImage.src = toy.image 
+      toyImage.className = 'toy-avatar'
+      divCard.appendChild(toyImage)
+
+      let toyLikes = document.createElement('p')
+      toyLikes.innerText = `${toy.likes} likes` 
+      divCard.appendChild(toyLikes)
+
+      let toyBtn = document.createElement('button')
+      toyBtn.className = 'like-btn'
+      toyBtn.id = 'toy-id'
+      toyBtn.innerText = 'Like'
+      divCard.appendChild(toyBtn)
+
+      toyBtn.addEventListener('click', (e) => {
+        let newLikes = toy.likes + 1
+        fetch(`http://localhost:3000/toys/${toy.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type' :'application/json'},
+          body: JSON.stringify ({
+            'likes': newLikes
+          })
+        })
+        .then((resp) => resp.json())
+        .then((element) => {
+          console.log(element)
+          let likes = element.likes
+          toyLikes.innerText = `${likes} likes` 
+        })
+      })
+    })
+  })
+  
+  let submitButton = document.querySelector("input[class='submit']")
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    let toyName = document.querySelector('input[name="name"]')
+    let toyUrl = document.querySelector('input[name="image"]')
+    let toyObj = {
+      'name': `${toyName.value}`,
+      'image': `${toyUrl.value}`,
+      'likes': 0
+    }
+
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: { 
+        'accept': 'application/json',
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(toyObj)
+    })
+    .then(resp => resp.json())
+    .then((data) => {
+      console.log(data)
+      let divCard = document.createElement('div')
+      divCard.className = 'card'
+      document.getElementById('toy-collection').appendChild(divCard)
+
+      let toyNameH2 = document.createElement('h2')
+      toyNameH2.innerText = data.name 
+
+      let toyImage = document.createElement('img')
+      toyImage.src = data.image 
+      toyImage.className = 'toy-avatar'
+
+      let toyLikes = document.createElement('p')
+      toyLikes.innerText = `${data.likes} likes` 
+      
+      
+      divCard.appendChild(toyNameH2)
+      divCard.appendChild(toyImage)
+      divCard.appendChild(toyLikes)
+
+      let toyBtn = document.createElement('button')
+      toyBtn.className = 'like-btn'
+      toyBtn.id = '[toy_id]'
+      toyBtn.innerText = 'Like'
+
+      divCard.appendChild(toyBtn)
+    })
+  })
+
+  /*let likeBtnUpdate = document.getElementsByClassName('like-btn')
+      let likeBtnArray = Array.from(likeBtnUpdate)
+      likeBtnArray.forEach((button) => {
+      button.addEventListener('click', (e) => {
+      console.log(e)
+    })
+  })*/
+})
+
+
+/*let likeBtnUpdate = document.getElementsByClassName('like-btn')
+Array.from(likeBtnUpdate)
+  for (let i = 0; i < likeBtnUpdate.length; i++) {
+    likeBtnUpdate[i].addEventListener('click', (e) => {
+      console.log(e)
+    })
+  }
+  
+
+  let likeBtnUpdate = document.getElementsByClassName('like-btn')
+  let likeBtnArray = Array.from(likeBtnUpdate)
+  likeBtnArray.forEach((button) => {
+    button.addEventListener('click', (e) => {
+    console.log(e)
+    })
+  })
+
+  
+  for (let i = 0; i < likeBtnUpdate.length; i++) {
+    likeBtnUpdate[i].addEventListener('click', (e) => {
+      console.log(e)
+    })
+  }*/
